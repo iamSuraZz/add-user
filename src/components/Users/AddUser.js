768a4f0c77cc862e8e6654722868df3978235aea
SuraZz007 <surajdev9886@gmail.com>
2022-08-27 15:10:13 +0530

    add basic functionalities of react in the app

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import Button from "../UI/Button";
+import Card from "../UI/Card";
+import ErrorModal from "../UI/ErrorModal";
+import styles from "./AddUser.module.css";
+
+const AddUser = (props) => {
+  const [enteredUserName, setEnteredUserName] = useState("");
+  const [enteredUserAge, setEnteredUserAge] = useState("");
+  const [error, setError] = useState();
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    if (
+      enteredUserName.trim().length === 0 ||
+      enteredUserAge.trim().length === 0
+    ) {
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid name and age (non-empty values).",
+      });
+      return;
+    }
+    if (+enteredUserAge < 1) {
+      setError({
+        title: "Invalid age",
+        message: "Please enter a valid age (> 0).",
+      });
+      return;
+    }
+    props.onAddUser(enteredUserName, enteredUserAge);
+    setEnteredUserName("");
+    setEnteredUserAge("");
+  };
+
+  const userNameChangeHandler = (event) => {
+    setEnteredUserName(event.target.value);
+  };
+
+  const userAgeChangeHandler = (event) => {
+    setEnteredUserAge(event.target.value);
+  };
+
+  const errorHandler = () => {
+    setError(null);
+  };
+
+  return (
+    <div>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
+
+      <Card className={styles.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            value={enteredUserName}
+            id="username"
+            onChange={userNameChangeHandler}
+          />
+
+          <label htmlFor="age">Age (Years)</label>
+          <input
+            type="number"
+            id="age"
+            onChange={userAgeChangeHandler}
+            value={enteredUserAge}
+          />
+
+          <Button type="submit"> Add User</B
---
+++ src/components/Users/UsersList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Card from "../UI/Card";
+import styles from "./UserList.module.css";
+
+const UsersList = (props) => {
+  return (
+    <Card className={styles.users}>
+      <ul>
+        {props.users.map((user, i) => (
+          <li key={user.id}>
+            {user.name} ({user.age} years old)
+          </li>
+        ))}
+      </ul>
+    </Card>
+  );
+};
+export default UsersList;
